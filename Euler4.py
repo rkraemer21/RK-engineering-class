8c7ff5e4e3080833724ed580fcd778d6b4b15f94
rkraemer21 <rkraemer21@bsmschool.org>
2020-04-23 18:15:41 -0500

    Euler part 2

--- Euler4.py
+++ Euler4.py
@@ -6,7 +6,6 @@ numbers = []
 
 for x in range(123456789, 987654321): 
     for y in range(2, x // 2):
-        #if '1' in x and '2' in x and '3' in x and '4' in x and '5' in x and '6' in x and '7' in x and '8' in x and '9' in x and 
         if (x % y) == 0:
             numbers.append(x)
 
